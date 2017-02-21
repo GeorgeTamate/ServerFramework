64d23ccbc14968ac7fd51f8cc84f27e74517d3ad
Keiko Tamate <Keiko Tamate>
2017-02-21 16:54:37 -0400

    Assignment 8 (Reflection) complete.

---
+++ src/PHttp/Application/IPHttpApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PHttp.Application
+{
+    public interface IPHttpApplication
+    {
+        void Start();
+
+        void ExecuteAction();
+
+        string Name { get; set; }
+
+        event PreApplicationStartMethod preApplicationStartMethod;
+
+        event ApplicationStartMethod applicationStartMethod;
+    }
+
+    public delegate void PreApplicationStartMethod(Type type, string method);
+    public delegate void ApplicationStartMethod(Type type, string method);
+
+
+}
